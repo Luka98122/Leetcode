73312d53646d2ef8a29b3e2afc50cd0e64cab8e9
luka98122 <luka98122@gmail.com>
2026-02-06 23:53:03 +0100

    Daily Leetcode, 0066, Easy

---
+++ problems/0066_Plus_One.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        didBreak = False
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]!=9:
+                digits[i]+=1
+                didBreak = True
+                break
+            else:
+                digits[i] = 0
+        if didBreak==False:
+            digits.insert(0,1)
+        return digits
+
+c = Solution()
+print(c.plusOne([9,9,9]))    
