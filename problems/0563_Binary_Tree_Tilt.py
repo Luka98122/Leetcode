5eeca00fa40dbde2e7798c5055b7a4b54a2eb0a4
Luka Markovic <luka98122@gmail.com>
2026-05-07 11:55:36 +0200

    typing fixes

--- problems/0563_Binary_Tree_Tilt.py
+++ problems/0563_Binary_Tree_Tilt.py
@@ -1,4 +1,10 @@
 from typing import Optional
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def findTilt(self, root: Optional[TreeNode]) -> int:
         self.total_tilt = 0
