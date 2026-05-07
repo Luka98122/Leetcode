53abdb4ac21f2180e398a0c14a1ae3309cb1cc4e
Luka Markovic <luka98122@gmail.com>
2026-05-07 11:08:48 +0200

    Added DP solution

--- problems/3751_Total_Waviness_of_Numbers_in_Range_I.py
+++ problems/3751_Total_Waviness_of_Numbers_in_Range_I.py
@@ -1,4 +1,23 @@
 class Solution:
+    def totalWavinessDP(self, num1: int, num2: int) -> int:
+        wavineses = [0]*100001
+        for i in range(100,100001):
+            r = i%10
+            rem = i//10
+            c = rem%10
+            l = (rem//10)%10
+            wave = 0
+            
+            if (c<l and c<r): wave=1
+            if (c>l and c>r): wave=1
+            
+            wavineses[i] = wave+wavineses[rem]
+        
+        for i in range(1,100001):
+            wavineses[i] += wavineses[i-1]
+        
+        return wavineses[num2]-wavineses[num1-1]
+    
     def totalWaviness(self, num1: int, num2: int) -> int:
         res = 0
         for num in range(num1,num2+1):
